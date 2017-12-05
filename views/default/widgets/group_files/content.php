78f26b72cc5ab4636e3b2b21ce37a1445351cc6c
Bart Jeukendrup <bart@jeukendrup.nl>
2017-12-05 13:14:15 +0100

    fix default write permissions in widget

--- views/default/widgets/group_files/content.php
+++ views/default/widgets/group_files/content.php
@@ -6,10 +6,15 @@ $widget = elgg_extract("entity", $vars);
 $container = $widget->getContainerEntity();
 $homeGuid = $widget->folder ? $widget->folder : $container->guid;
 
+// remove widget context, as the widget_manager otherwise overwrites get_write_access_array() in plugin hook
+elgg_pop_context();
+$accessIds = get_write_access_array();
+elgg_push_context("widgets");
+
 $data = array(
     'containerGuid' => $container->guid,
     'homeGuid' => $homeGuid,
-    'accessIds' => get_write_access_array(),
+    'accessIds' => $accessIds,
     'isWidget' => true,
     'odt_enabled' => elgg_is_active_plugin('odt_editor') ? true : false,
     'limit' => $widget->limit ? $widget->limit : 10
